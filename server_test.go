54439a336a9ee8f373d46e4ab0313448e15a769f
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2022-09-25 13:32:05 -0400

    Added additional unit tests to server_test.go

--- server_test.go
+++ server_test.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func TestGetKeyNotFound(t *testing.T) {
+	router := mux.NewRouter()
+	router.HandleFunc("/keys/{key}", GetKeyHandlerFunc)
+	req, err := http.NewRequest("GET", "/keys/key1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("Handler returned wrong status code: got %v expected %v",
+			status, http.StatusNotFound)
+	}
+
+	if rr.Body.String() != "" {
+		t.Errorf("handler returned unexpected body: got %v expected %v",
+			rr.Body.String(), "")
+	}
+}
 
 func TestHandlerEmptyGetAll(t *testing.T) {
 	req, err := http.NewRequest("GET", "/keys", nil)
@@ -64,5 +88,134 @@ func TestHandlerGetAll(t *testing.T) {
 		if !strings.Contains(resBdy, res) {
 			t.Errorf("Res Body does not contain expected value: %s", res)
 		}
+	}
+}
+
+func TestHandlerFoundKey(t *testing.T) {
+	key := "TestKey"
+	value := "TestVal"
+
+	err := Put(key, value)
+	if err != nil {
+		t.Errorf("Failure adding test pair")
+	}
+
+	path := fmt.Sprintf("/keys/%s", key)
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/keys/{key}", GetKeyHandlerFunc)
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Error - Expected StatusCode %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	resBdy := rr.Body.String()
+	if resBdy != value+"\n" {
+		t.Errorf("Incorrect value returned for key: %s, got %s, expected %s", key, resBdy, value)
+	}
+}
+
+func TestPostHandlerFoundKey(t *testing.T) {
+	key := "TestKey2"
+	value := "TestVal2"
+
+	router := mux.NewRouter()
+	router.HandleFunc("/keys/{key}", GetKeyHandlerFunc)
+	router.HandleFunc("/keys", AddKeyHandlerFunc).Methods("POST")
+
+	reqBody := fmt.Sprintf(`{"key": "%s", "value": "%s"}`, key, value)
+	reqCreate, err := http.NewRequest("POST", "/keys", bytes.NewBuffer([]byte(reqBody)))
+	reqCreate.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatalf("Got Error when POST'ing: %v", err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, reqCreate)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("Error - Expected StatusCode Created %d, got %d", http.StatusCreated, rr.Code)
+	}
+
+	reqGet, err := http.NewRequest("GET", fmt.Sprintf("/keys/%s", key), nil)
+	if err != nil {
+		t.Fatalf("Error: %v during request to get %s", err.Error(), key)
+	}
+
+	rr = httptest.NewRecorder()
+	router.ServeHTTP(rr, reqGet)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Error - Expected StatusCode OK %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	resBdy := rr.Body.String()
+	if resBdy != value+"\n" {
+		t.Errorf("Incorrect value returned for key: %s, got %s, expected %s", key, resBdy, value)
+	}
+}
+
+func TestPostHandlerUpdate(t *testing.T) {
+	key := "TestKey3"
+	value := "TestVal3"
+	updateVal := "Update3"
+
+	router := mux.NewRouter()
+	router.HandleFunc("/keys/{key}", GetKeyHandlerFunc)
+	router.HandleFunc("/keys", AddKeyHandlerFunc).Methods("POST", "PUT")
+
+	reqBody := fmt.Sprintf(`{"key":"%s","value":"%s"}`, key, value)
+	reqCreate, err := http.NewRequest("POST", "/keys", bytes.NewBuffer([]byte(reqBody)))
+	reqCreate.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatalf("Got Error when POST'ing: %v", err.Error())
+	}
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, reqCreate)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("Error - Expected StatusCode Created %d, got %d", http.StatusCreated, rr.Code)
+	}
+
+	// test PUT update
+	reqBody = fmt.Sprintf(`{"key":"%s","value":"%s"}`, key, updateVal)
+	reqPut, err := http.NewRequest("PUT", "/keys", bytes.NewBuffer([]byte(reqBody)))
+	reqPut.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatalf("Error: %v during request to PUT %s", err.Error(), key)
+	}
+
+	rr = httptest.NewRecorder()
+	router.ServeHTTP(rr, reqPut)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Error - Expected StatusCode OK %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	resBdy := rr.Body.String()
+	if resBdy != reqBody {
+		t.Errorf("Incorrect value returned for key: %s, got %s, expected %s", key, resBdy, reqBody)
+	}
+
+	// test GET - the updated value is returned
+	reqGet, err := http.NewRequest("GET", fmt.Sprintf("/keys/%s", key), nil)
+	if err != nil {
+		t.Fatalf("Error: %v during request to get %s", err.Error(), key)
+	}
+
+	rr = httptest.NewRecorder()
+	router.ServeHTTP(rr, reqGet)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Error - Expected StatusCode OK %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	resBdy = rr.Body.String()
+	if resBdy != updateVal+"\n" {
+		t.Errorf("Incorrect value returned for key: %s, got %s, expected %s", key, resBdy, updateVal)
 	}
 }
